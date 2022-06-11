6a6c9f0e1f9fc78d127fc30798883614ed6b941a
Russell Joyce <russell.joyce@york.ac.uk>
2022-06-11 19:39:43 +0100

    Fixed apt update from York CS Pi-puck package repository
    Added an exception to apt so there is no OCSP check on www.cs.york.ac.uk
    repository. Without this exception, pi-gen hits some certificate erorrs
    during build.

--- stage2+pi-puck/00-configure-apt/00-run.sh
+++ stage2+pi-puck/00-configure-apt/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 install -m 644 files/pi-puck.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
+install -m 644 files/99pi-puck-cert "${ROOTFS_DIR}/etc/apt/apt.conf.d/"
 
 on_chroot apt-key add - < files/pi-puck.gpg.key
 on_chroot << EOF
