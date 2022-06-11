a83dfd5b403e8870ead779a99b052abcf3e73058
Alan Millard <alan.millard@york.ac.uk>
2022-06-11 21:58:03 +0100

    Configured network, added packages, and cloning git repos

---
+++ stage2+pi-puck/04-swarmhack/00-run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+git clone https://github.com/yorkrobotlab/pi-puck.git "${ROOTFS_DIR}/home/pi"
+git clone https://github.com/yorkrobotlab/pi-puck-e-puck1.git "${ROOTFS_DIR}/home/pi"
