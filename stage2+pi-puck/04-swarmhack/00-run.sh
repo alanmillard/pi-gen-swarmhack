debbff9389796568f3d68b6e96a9f8a2b7115227
Alan Millard <millard.alan@gmail.com>
2022-06-11 22:05:19 +0100

    Skipping desktop build stages, removed unnecessary packages, and installing Pi-puck Python library via pip

--- stage2+pi-puck/04-swarmhack/00-run.sh
+++ stage2+pi-puck/04-swarmhack/00-run.sh
@@ -2,3 +2,4 @@
 
 git clone https://github.com/yorkrobotlab/pi-puck.git "${ROOTFS_DIR}/home/pi"
 git clone https://github.com/yorkrobotlab/pi-puck-e-puck1.git "${ROOTFS_DIR}/home/pi"
+pip3 install pipuck
