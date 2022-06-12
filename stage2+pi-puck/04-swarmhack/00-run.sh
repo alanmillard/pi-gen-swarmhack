408a130ab2f2cd0e338977189133f5114deaa839
Alan Millard <alan.millard@york.ac.uk>
2022-06-12 02:21:07 +0100

    Removed networking packages, and attempting to fix git clone and pip install

--- stage2+pi-puck/04-swarmhack/00-run.sh
+++ stage2+pi-puck/04-swarmhack/00-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-git clone https://github.com/yorkrobotlab/pi-puck.git "${ROOTFS_DIR}/home/pi"
-git clone https://github.com/yorkrobotlab/pi-puck-e-puck1.git "${ROOTFS_DIR}/home/pi"
-pip3 install pipuck
+git clone https://github.com/yorkrobotlab/pi-puck.git "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/pi-puck"
+git clone https://github.com/yorkrobotlab/pi-puck-e-puck1.git "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/pi-puck-e-puck1"
+yes | pip3 install pipuck
