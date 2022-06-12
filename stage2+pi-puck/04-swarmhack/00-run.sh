fff0cbcecb1a504d8730deca7bb6ece366d306a4
Alan Millard <alan.millard@york.ac.uk>
2022-06-12 13:47:48 +0100

    Fixed pip package installation

--- stage2+pi-puck/04-swarmhack/00-run.sh
+++ stage2+pi-puck/04-swarmhack/00-run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -e
 
+echo "[SWARMHACK] on_chroot start"
+
+on_chroot << EOF
+echo "[SWARMHACK] Cloning pi-puck git repo"
 git clone https://github.com/yorkrobotlab/pi-puck.git "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/pi-puck"
+echo "[SWARMHACK] Cloning pi-puck-e-puck1 git repo"
 git clone https://github.com/yorkrobotlab/pi-puck-e-puck1.git "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/pi-puck-e-puck1"
+echo "[SWARMHACK] Installing pipuck Python library"
 yes | pip3 install pipuck
+EOF
+
+echo "[SWARMHACK] on_chroot end"
